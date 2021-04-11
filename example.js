de9205d8f6054ec73768060bfed9db39cdf9c3d8
rdk <66311944+dvuhvostka@users.noreply.github.com>
2021-04-11 16:25:34 +0300

    Comments

--- example.js
+++ example.js
@@ -7,7 +7,7 @@ async function example1(){
   const res = await SWAP.swapExactETHForTokens( // Поменять BNB на токены
     0.01, // Сколько BNB меняем
     '0xDA21A245BACef568DF3f9CCF445C43889B59E3C4', // Адрес кошелька
-    '0x3137643D0a4308df08A7B77e61C657592Cc1687f' // На что меняем (В данном случае INDA TOKENs)
+    '0x3137643D0a4308df08A7B77e61C657592Cc1687f' // На что меняем (В данном случае INDA TOKEN)
     );
   return res;
 }
@@ -16,8 +16,8 @@ async function example2(){
   const res = await SWAP.swapExactTokensForTokens( // Поменять токены на токены
     0.01, // Сколько меняем
     '0xDA21A245BACef568DF3f9CCF445C43889B59E3C4', // Адрес кошелька
-    '0x3137643D0a4308df08A7B77e61C657592Cc1687f', // Что меняем (INDA Token)
-    '0x9B3726D10DF962C1D35bfCA09F7F86ADC7022568', // На что меняем (BUSD Token)
+    '0x3137643D0a4308df08A7B77e61C657592Cc1687f', // Что меняем (например: INDA Token)
+    '0x9B3726D10DF962C1D35bfCA09F7F86ADC7022568', // На что меняем (например: BUSD Token)
     2 // decimals того, что меняем
     );
   return res;
@@ -26,7 +26,7 @@ async function example2(){
 async function example3(){
   const res = await SWAP.getBalance( // Получить баланс токена
     '0xDA21A245BACef568DF3f9CCF445C43889B59E3C4', // Адрес кошелька
-    '0x3137643D0a4308df08A7B77e61C657592Cc1687f',  // INDA Token
+    '0x3137643D0a4308df08A7B77e61C657592Cc1687f',  // Адрес токена (В данном случае INDA TOKENs)
     2 // decimals INDA
   );
   return res;
@@ -34,8 +34,8 @@ async function example3(){
 
 async function example4(){
   const res = await SWAP.getLPBalance( // Получить баланс LP токенов (пары).
-    '0x3137643D0a4308df08A7B77e61C657592Cc1687f', // Адрес первого токена (INDA Token)
-    '0x9b3726d10df962c1d35bfca09f7f86adc7022568', // Адрес второго токена (BUSD Token)
+    '0x3137643D0a4308df08A7B77e61C657592Cc1687f', // Адрес первого токена (например: INDA Token)
+    '0x9b3726d10df962c1d35bfca09f7f86adc7022568', // Адрес второго токена (например: BUSD Token)
     '0xDA21A245BACef568DF3f9CCF445C43889B59E3C4' // Адрес кошелька 
     )
   return res;
@@ -43,10 +43,10 @@ async function example4(){
 
 async function example5(){
   const res = await SWAP.addLiquidityETH( // Добавить ликвидность (Token / BNB)
-    '0x3137643D0a4308df08A7B77e61C657592Cc1687f', // INDA Token
+    '0x3137643D0a4308df08A7B77e61C657592Cc1687f', // Адрес токена который добавляем (например: INDA Token)
     '0xDA21A245BACef568DF3f9CCF445C43889B59E3C4', // Адрес кошелька на котором добавляем ликвидку
-    1, // Кол-во INDA
-    2 // decimals INDA
+    1, // Кол-во токена (в данном случае, количество INDA)
+    2 // decimals токена (у INDA Token, decimals = 2)
   );
   return res;
 }
@@ -54,13 +54,13 @@ async function example5(){
 async function example6() {
     const res = await SWAP.addLiquidity( // Добавить ликвидность (Token / Token)
     {
-      address: '0x3137643D0a4308df08A7B77e61C657592Cc1687f', // INDA Token
-      amount: 1, // Кол-во инды
-      decimals: 2 // decimals инды
+      address: '0x3137643D0a4308df08A7B77e61C657592Cc1687f', // Адрес токена (например: INDA Token)
+      amount: 1, // Кол-во токена
+      decimals: 2 // decimals токена (У INDA Token, decimals = 2)
     },
-    '0x9b3726d10df962c1d35bfca09f7f86adc7022568', // BUSD Token
+    '0x9b3726d10df962c1d35bfca09f7f86adc7022568', // Адрес второго токена (например: BUSD Token)
     '0xDA21A245BACef568DF3f9CCF445C43889B59E3C4', // Адрес кошелька на котором делаем swap
-    18 // decimals BUSD
+    18 // decimals второго токена (у BUSD Token, decimals = 18)
   )
   return res;
 }
