866df5d17c633b357ab42b0f0ef0336cc3c31228
rdk <66311944+dvuhvostka@users.noreply.github.com>
2021-04-11 16:19:41 +0300

    added new funcs and updated readme

--- example.js
+++ example.js
@@ -4,40 +4,63 @@ window.ethereum.enable()
 const SWAP = new Swap()
 
 async function example1(){
-  const res = await SWAP.swapExactETHForTokens(
-    0.01, 
-    '0xDA21A245BACef568DF3f9CCF445C43889B59E3C4', 
-    '0x3137643D0a4308df08A7B77e61C657592Cc1687f'
+  const res = await SWAP.swapExactETHForTokens( // Поменять BNB на токены
+    0.01, // Сколько BNB меняем
+    '0xDA21A245BACef568DF3f9CCF445C43889B59E3C4', // Адрес кошелька
+    '0x3137643D0a4308df08A7B77e61C657592Cc1687f' // На что меняем (В данном случае INDA TOKENs)
     );
   return res;
 }
 
 async function example2(){
-  const res = await SWAP.swapExactTokensForTokens(
-    0.01, 
-    '0xDA21A245BACef568DF3f9CCF445C43889B59E3C4', 
-    '0x3137643D0a4308df08A7B77e61C657592Cc1687f', 
-    '0x9B3726D10DF962C1D35bfCA09F7F86ADC7022568', 
-    2
+  const res = await SWAP.swapExactTokensForTokens( // Поменять токены на токены
+    0.01, // Сколько меняем
+    '0xDA21A245BACef568DF3f9CCF445C43889B59E3C4', // Адрес кошелька
+    '0x3137643D0a4308df08A7B77e61C657592Cc1687f', // Что меняем (INDA Token)
+    '0x9B3726D10DF962C1D35bfCA09F7F86ADC7022568', // На что меняем (BUSD Token)
+    2 // decimals того, что меняем
     );
   return res;
 }
 
 async function example3(){
-  const res = await SWAP.getBalance(
-    '0xDA21A245BACef568DF3f9CCF445C43889B59E3C4', 
-    '0x3137643D0a4308df08A7B77e61C657592Cc1687f', 
-    2
+  const res = await SWAP.getBalance( // Получить баланс токена
+    '0xDA21A245BACef568DF3f9CCF445C43889B59E3C4', // Адрес кошелька
+    '0x3137643D0a4308df08A7B77e61C657592Cc1687f',  // INDA Token
+    2 // decimals INDA
   );
   return res;
 }
 
 async function example4(){
-  const res = await SWAP.addLiquidityETH(
-    '0x3137643D0a4308df08A7B77e61C657592Cc1687f',
-    '0xDA21A245BACef568DF3f9CCF445C43889B59E3C4',
-    1,
-    2
+  const res = await SWAP.getLPBalance( // Получить баланс LP токенов (пары).
+    '0x3137643D0a4308df08A7B77e61C657592Cc1687f', // Адрес первого токена (INDA Token)
+    '0x9b3726d10df962c1d35bfca09f7f86adc7022568', // Адрес второго токена (BUSD Token)
+    '0xDA21A245BACef568DF3f9CCF445C43889B59E3C4' // Адрес кошелька 
+    )
+  return res;
+}
+
+async function example5(){
+  const res = await SWAP.addLiquidityETH( // Добавить ликвидность (Token / BNB)
+    '0x3137643D0a4308df08A7B77e61C657592Cc1687f', // INDA Token
+    '0xDA21A245BACef568DF3f9CCF445C43889B59E3C4', // Адрес кошелька на котором добавляем ликвидку
+    1, // Кол-во INDA
+    2 // decimals INDA
   );
+  return res;
+}
+
+async function example6() {
+    const res = await SWAP.addLiquidity( // Добавить ликвидность (Token / Token)
+    {
+      address: '0x3137643D0a4308df08A7B77e61C657592Cc1687f', // INDA Token
+      amount: 1, // Кол-во инды
+      decimals: 2 // decimals инды
+    },
+    '0x9b3726d10df962c1d35bfca09f7f86adc7022568', // BUSD Token
+    '0xDA21A245BACef568DF3f9CCF445C43889B59E3C4', // Адрес кошелька на котором делаем swap
+    18 // decimals BUSD
+  )
   return res;
 }
--- swap/swap.js
+++ swap/swap.js
@@ -7,11 +7,32 @@ import { factoryabi } from './ABI/factory.js'
 import { pairabi } from './ABI/pair.js'
 
 const web3 = new Web3(window.web3.currentProvider)
+const web3Version = 97 // Для прода поменять на 56
 
-const PANCAKE_FACTORY_ADDR = '0xb3bED7c8814DD91dF8e521B154c7c11A0d867822' // Pancake Factory Testnet
-const PANCAKE_ROUTER_ADDR = '0x4fb3e9656055B520950eC0ED0b45651bc21Ff697' // Pancake Router Testnet
+let PANCAKE_FACTORY_ADDR
+let PANCAKE_ROUTER_ADDR
+let WBNBToken
 
-const WBNBToken = '0xae13d989daC2f0dEbFf460aC112a837C89BAa7cd' // WBNB Token Testnet
+switch (web3Version) {
+  case 97: {
+    PANCAKE_FACTORY_ADDR = '0xb3bED7c8814DD91dF8e521B154c7c11A0d867822' // Pancake Factory Testnet
+    PANCAKE_ROUTER_ADDR = '0x4fb3e9656055B520950eC0ED0b45651bc21Ff697' // Pancake Router Testnet
+    WBNBToken = '0xae13d989daC2f0dEbFf460aC112a837C89BAa7cd' // WBNB Token Testnet
+    break
+  }
+  case 56: {
+    PANCAKE_FACTORY_ADDR = '0xBCfCcbde45cE874adCB698cC183deBcF17952812' // Pancake Factory Mainnet
+    PANCAKE_ROUTER_ADDR = '0x05fF2B0DB69458A0750badebc4f9e13aDd608C7F' // Pancake Router Mainnet
+    WBNBToken = '0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c' // WBNB Token Mainnet
+    break
+  }
+  default: {
+    PANCAKE_FACTORY_ADDR = 0
+    PANCAKE_ROUTER_ADDR = 0
+    WBNBToken = 0
+    break
+  }
+}
 
 export class Swap {
   constructor () {
@@ -19,6 +40,35 @@ export class Swap {
 
     this.indaswap = new web3.eth.Contract(routerabi, PANCAKE_ROUTER_ADDR) // создали новый инстанс роутера.
     this.factory = new web3.eth.Contract(factoryabi, PANCAKE_FACTORY_ADDR) // создали новый инстанс фактори.
+  }
+
+  async getPair (token1, token2) {
+    const pairAddr = await this.factory.methods.getPair(token1, token2).call()
+    return pairAddr
+  }
+
+  async getRate (tokenA, tokenB) {
+    const pairaddr = await this.getPair(tokenA, tokenB)
+    const currentPairContract = new web3.eth.Contract(pairabi, pairaddr)
+    const reserves = await currentPairContract.methods.getReserves().call()
+    const token = reserves._reserve0 / Math.pow(10, 2)
+    const eth = reserves._reserve1 / Math.pow(10, 18)
+
+    this.rate = token / eth
+
+    return this.rate
+  }
+
+  async getLPBalance (tokenA, tokenB, address) {
+    const pairaddr = await this.getPair(tokenA, tokenB)
+    console.log(pairaddr)
+    const currentPairContract = new web3.eth.Contract(pairabi, pairaddr)
+    const balance = await currentPairContract.methods.balanceOf(web3.utils.toChecksumAddress(address)).call()
+    return balance / 1e18
+  }
+
+  getProvider () {
+    return window.web3
   }
 
   async getBalance (usrAddr, tokenAddr, decimals) {
@@ -42,28 +92,8 @@ export class Swap {
     })
   }
 
-  async getPair (token1, token2) {
-    const pairAddr = await this.factory.methods.getPair(token1, token2).call()
-    return pairAddr
-  }
-
-  async getRate (tokenA, tokenB) {
-    const pairaddr = await this.getPair(tokenA, tokenB)
-    const currentPairContract = new web3.eth.Contract(pairabi, pairaddr)
-    const reserves = await currentPairContract.methods.getReserves().call()
-    const token = reserves._reserve0 / Math.pow(10, 2)
-    const eth = reserves._reserve1 / Math.pow(10, 18)
-
-    this.rate = token / eth
-
-    return this.rate
-  }
-
   async addLiquidityETH (tokenA, buyer, amount, decimals) {
-    // const pairAddr = await this.factory.methods.getPair(tokenA, tokenB).call()
-
     const rate = await this.getRate(tokenA, WBNBToken)
-    console.log(rate)
     return new Promise(async (resolve, reject) => {
       const bnb = ((((amount / (rate)) * 0.995)) * 1e18).toFixed()
       const gas = await this.indaswap.methods.addLiquidityETH(
@@ -89,6 +119,41 @@ export class Swap {
         from: buyer,
         gas: gas,
         value: bnb
+      })
+      resolve(res)
+    })
+  }
+
+  async addLiquidity (tokenA, tokenB, buyer, decimals) {
+    const rate = await this.getRate(tokenA.address, tokenB)
+    return new Promise(async (resolve, reject) => {
+      const amountB = ((tokenA.amount / rate) * Math.pow(10, decimals))
+      const amountA = tokenA.amount * Math.pow(10, tokenA.decimals)
+      const gas = await this.indaswap.methods.addLiquidity(
+        tokenA.address,
+        tokenB,
+        amountA.toFixed().toString(),
+        amountB.toFixed().toString(),
+        (amountA * 0.99).toFixed().toString(),
+        (amountB * 0.99).toFixed().toString(),
+        buyer,
+        Math.floor(new Date().getTime() / 1000) + 1200
+      ).estimateGas({
+        from: buyer,
+        gas: '0x' + Number(3000000).toString(16)
+      })
+      const res = this.indaswap.methods.addLiquidity(
+        tokenA.address,
+        tokenB,
+        amountA.toFixed().toString(),
+        amountB.toFixed().toString(),
+        (amountA * 0.99).toFixed().toString(),
+        (amountB * 0.99).toFixed().toString(),
+        buyer,
+        Math.floor(new Date().getTime() / 1000) + 1200
+      ).send({
+        from: buyer,
+        gas: gas
       })
       resolve(res)
     })
