d1d2ff45fb747a7a860c181e7627cc83b035243d
rdk <66311944+dvuhvostka@users.noreply.github.com>
2021-04-11 21:47:08 +0300

    updated funcs

--- swap/swap.js
+++ swap/swap.js
@@ -3,20 +3,23 @@ import * as Web3 from 'web3'
 
 import { routerabi } from './ABI/router.js' // ABI PancakeRouter
 import { tokenabi } from './ABI/token.js' // ABI Token
-import { factoryabi } from './ABI/factory.js'
-import { pairabi } from './ABI/pair.js'
+import { factoryabi } from './ABI/factory.js' // ABI PancakeFactory
+import { pairabi } from './ABI/pair.js' // ABI Pair
+import { liquidityabi } from './ABI/liquidity.js' // ABI Liqidity
 
 const web3 = new Web3(window.web3.currentProvider)
 const web3Version = 97 // Для прода поменять на 56
 
 let PANCAKE_FACTORY_ADDR
 let PANCAKE_ROUTER_ADDR
+let LIQUIDITY_ADRR
 let WBNBToken
 
 switch (web3Version) {
   case 97: {
     PANCAKE_FACTORY_ADDR = '0xb3bED7c8814DD91dF8e521B154c7c11A0d867822' // Pancake Factory Testnet
     PANCAKE_ROUTER_ADDR = '0x4fb3e9656055B520950eC0ED0b45651bc21Ff697' // Pancake Router Testnet
+    LIQUIDITY_ADRR = '0x26c8067959E6B3FF489dF9d781a7c79bBdb4dCd6' // Liquidity
     WBNBToken = '0xae13d989daC2f0dEbFf460aC112a837C89BAa7cd' // WBNB Token Testnet
     break
   }
@@ -29,6 +32,7 @@ switch (web3Version) {
   default: {
     PANCAKE_FACTORY_ADDR = 0
     PANCAKE_ROUTER_ADDR = 0
+    LIQUIDITY_ADRR = 0
     WBNBToken = 0
     break
   }
@@ -37,7 +41,7 @@ switch (web3Version) {
 export class Swap {
   constructor () {
     this.address = PANCAKE_ROUTER_ADDR // Сохранили адрес роутера в константу
-
+    this.liquidity = new web3.eth.Contract(liquidityabi, LIQUIDITY_ADRR) // создали новый инстанс ликвидити.
     this.indaswap = new web3.eth.Contract(routerabi, PANCAKE_ROUTER_ADDR) // создали новый инстанс роутера.
     this.factory = new web3.eth.Contract(factoryabi, PANCAKE_FACTORY_ADDR) // создали новый инстанс фактори.
   }
@@ -61,7 +65,6 @@ export class Swap {
 
   async getLPBalance (tokenA, tokenB, address) {
     const pairaddr = await this.getPair(tokenA, tokenB)
-    console.log(pairaddr)
     const currentPairContract = new web3.eth.Contract(pairabi, pairaddr)
     const balance = await currentPairContract.methods.balanceOf(web3.utils.toChecksumAddress(address)).call()
     return balance / 1e18
@@ -88,6 +91,101 @@ export class Swap {
         ]
       ).call()
       console.log('expected return: ', res)
+      resolve(res)
+    })
+  }
+
+  async getLiquidityValue (tokenA, tokenB, amount) {
+    return new Promise((resolve, reject) => {
+      const res = this.liquidity.methods.getLiquidityValue(
+        web3.utils.toChecksumAddress(tokenA),
+        web3.utils.toChecksumAddress(tokenB),
+        (amount * 1e18).toFixed().toString()
+      ).call()
+      resolve(res)
+    })
+  }
+
+  async approveToken (address, buyer) {
+    const currentContract = new web3.eth.Contract(tokenabi, address)
+    return new Promise(async (resolve, reject) => {
+      const gas = await currentContract.methods.approve(PANCAKE_ROUTER_ADDR, '0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff').estimateGas({
+        from: buyer,
+        gas: '0x' + Number(3000000).toString(16)
+      })
+
+      const res = await currentContract.methods.approve(PANCAKE_ROUTER_ADDR, '0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff').send({
+        from: buyer,
+        gas: gas
+      })
+      resolve(res)
+    })
+  }
+
+  async getAllowance (address, buyer) {
+    const currentContract = new web3.eth.Contract(tokenabi, address)
+    return new Promise(async (resolve, reject) => {
+      const res = await currentContract.methods.allowance(buyer, PANCAKE_ROUTER_ADDR).call()
+      resolve(res)
+    })
+  }
+
+  async removeLiquidityETH (token, amount, buyer) {
+    const liqValue = await this.getLiquidityValue(token, WBNBToken, amount)
+    return new Promise((resolve, reject) => {
+      const gas = this.indaswap.methods.removeLiquidityETH(
+        token,
+        (amount * 1e18).toFixed().toString(),
+        (liqValue.tokenAAmount * 0.99).toFixed().toString(),
+        (liqValue.tokenBAmount * 0.99).toFixed().toString(),
+        buyer,
+        Math.floor(new Date().getTime() / 1000) + 1200
+      ).estimateGas({
+        from: buyer,
+        gas: '0x' + Number(3000000).toString(16)
+      })
+      const res = this.indaswap.methods.removeLiquidityETH(
+        token,
+        (amount * 1e18).toFixed().toString(),
+        (liqValue.tokenAAmount * 0.99).toFixed().toString(),
+        (liqValue.tokenBAmount * 0.99).toFixed().toString(),
+        buyer,
+        Math.floor(new Date().getTime() / 1000) + 1200
+      ).send({
+        from: buyer,
+        gas: gas
+      })
+      resolve(res)
+    })
+  }
+
+  async removeLiquidity (tokenA, tokenB, amount, buyer) {
+    const liqValue = await this.getLiquidityValue(tokenA, tokenB, amount)
+    return new Promise((resolve, reject) => {
+      const gas = this.indaswap.methods.removeLiquidity(
+        tokenA,
+        tokenB,
+        (amount * 1e18).toFixed().toString(),
+        (liqValue.tokenAAmount * 0.99).toFixed().toString(),
+        (liqValue.tokenBAmount * 0.99).toFixed().toString(),
+        buyer,
+        Math.floor(new Date().getTime() / 1000) + 1200
+      ).estimateGas({
+        from: buyer,
+        gas: '0x' + Number(3000000).toString(16)
+      })
+      const res = this.indaswap.methods.removeLiquidity(
+        tokenA,
+        tokenB,
+        (amount * 1e18).toFixed().toString(),
+        (liqValue.tokenAAmount * 0.99).toFixed().toString(),
+        (liqValue.tokenBAmount * 0.99).toFixed().toString(),
+        buyer,
+        Math.floor(new Date().getTime() / 1000) + 1200
+      ).send({
+        from: buyer,
+        gas: gas
+      })
       resolve(res)
     })
   }
